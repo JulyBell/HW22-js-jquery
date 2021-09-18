1e6b04aaba2986bc1c36f8ea3a92624365b5566a
julybell <julybell16@gmail.com>
2021-09-18 19:47:45 +0300

    jquery tasks

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ let testClassArray = $(".test").css( 'backgroundColor', 'red' );
 
 let testClassHeaders = $("h2.test").css( 'backgroundColor', 'red' );
 
-let paragraphs = $("p").next("p").css( 'backgroundColor', 'red' );
+let paragraphs = $("p + p").css( 'backgroundColor', 'red' );
 
-let wwwLi = $("li.www").next("li").css( 'backgroundColor', 'red' );
+let wwwLi = $("li.www + li").css( 'backgroundColor', 'red' );
 
-let headerIs = $("h2").find("i").css( 'backgroundColor', 'red' );
+let headerIs = $("h2 i").css( 'backgroundColor', 'yellow' );
 
 let headerDirectChild = $("h2 > i").css( 'backgroundColor', 'red' );
 
@@ -16,8 +16,8 @@ let headerDirectChild = $("h2 > i").css( 'backgroundColor', 'red' );
 
 let headersExclTest = $("h2").not(".test").css( 'backgroundColor', 'red' );
 
-let notTestHeaders = $(":header").css( 'backgroundColor', 'red' );
+let notTestHeaders = $(":header").not(".test").css( 'backgroundColor', 'red' );
 
-let headersAfterHeaders = $(":header").next(":header").css( 'backgroundColor', 'red' );
+let headersAfterHeaders = $(":header + :header").css( 'backgroundColor', 'red' );
 
-let testHeadersAfterHeaders = $(":header.test").next(":header.test").css( 'backgroundColor', 'red' );
+let testHeadersAfterHeaders = $(":header.test + :header.test").css( 'backgroundColor', 'red' );
